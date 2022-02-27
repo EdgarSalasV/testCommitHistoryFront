a885d47215d6bf385dd0d8c61068a088124110e4
EdgarSalasBT <edgar.salas@avanttia.com>
2022-02-27 13:31:36 -0600

    added layout components with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './index.css';
+import Layout from "./components/layout";
+import "./index.css";
 
 export default function App() {
   return (
-    <p className="text-indigo-700 text-2xl font-bold p-4">
-      Hello world!
-    </p>
-  )
+    <div>
+      <Layout>
+        <p className="text-indigo-700 text-2xl font-bold p-4">Hello world!</p>
+      </Layout>
+    </div>
+  );
 }
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,16 @@
+const Layout = () => {
+  return (
+    <footer
+      className="bg-gray-200 text-center lg:text-left fixed inset-x-0 bottom-0"
+    >
+      <div className="text-gray-700 text-center p-4 bg-gray-400">
+        © 2022 Copyright:
+        <a className="text-gray-800">
+          Test Corporation
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Layout;
