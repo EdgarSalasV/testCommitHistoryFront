67a1058830df8991f604b838bb49fb26ec4cda4b
EdgarSalasBT <edgar.salas@avanttia.com>
2022-02-28 13:23:04 -0600

    added @apply classes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import '../../styles/layout.css';
+import "../../styles/layout.css";
 
 const Header = () => {
   return (
@@ -46,11 +46,10 @@ const Header = () => {
             </a>
           </div>
           <div>
-            <button
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0"
-            >
+            <a className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">
               Account
-            </button>
+            </a>
+            <button className="btn btn-blue">Button</button>
           </div>
         </div>
       </nav>
